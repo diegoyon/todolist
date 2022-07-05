5e2fa8bdbd8f0e605d25301beb383e78728e00a4
diego <diegoyon@outlook.com>
2022-07-05 13:00:35 -0600

    Set up linters and fix linters issues

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-import _ from "lodash";
-import "./style.css";
-import Vert from "../icons/vert.png";
+import './style.css';
+import Vert from '../icons/vert.png';
 
-let iconVert = new Image();
+const iconVert = new Image();
 iconVert.src = Vert;
 
-let toDoTasks = [
+const toDoTasks = [
   {
-    description: "wash the car",
+    description: 'wash the car',
     bool: false,
     index: 1,
   },
   {
-    description: "buy food",
+    description: 'buy food',
     bool: false,
     index: 2,
   },
   {
-    description: "send email",
+    description: 'send email',
     bool: false,
     index: 3,
   },
 ];
 
-const list = document.querySelector("ul");
+const list = document.querySelector('ul');
 
 function displayTasks(arr) {
-  //Sorts the array of objects according to their index
+  // Sorts the array of objects according to their index
   toDoTasks.sort((a, b) => a.index - b.index);
 
-  //Iterates the array and displays them
-  for (let i = 0; i < arr.length; i++) {
-    const item = document.createElement("li");
+  // Iterates the array and displays them
+  for (let i = 0; i < arr.length; i += 1) {
+    const item = document.createElement('li');
     item.innerHTML = `<div><input type="checkbox"><p>${arr[i].description}</p></div><img src="${iconVert.src}" alt="vert" />`;
     list.appendChild(item);
   }
