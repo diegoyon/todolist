208795e91dc152faeeb7c351df3464342b6c276c
diego <diegoyon@outlook.com>
2022-07-05 13:38:10 -0600

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import "./style.css";
-import Vert from "../icons/vert.png";
+import './style.css';
+import Vert from '../icons/vert.png';
 
 const iconVert = new Image();
 iconVert.src = Vert;
 
 const toDoTasks = [
   {
-    description: "wash the car",
+    description: 'wash the car',
     completed: false,
     index: 1,
   },
   {
-    description: "buy food",
+    description: 'buy food',
     completed: false,
     index: 2,
   },
   {
-    description: "send email",
+    description: 'send email',
     completed: false,
     index: 3,
   },
 ];
 
-const list = document.querySelector("ul");
+const list = document.querySelector('ul');
 
 function displayTasks(arr) {
   // Sorts the array of objects according to their index
@@ -30,7 +30,7 @@ function displayTasks(arr) {
 
   // Iterates the array and displays them
   for (let i = 0; i < arr.length; i += 1) {
-    const item = document.createElement("li");
+    const item = document.createElement('li');
     item.innerHTML = `<div><input type="checkbox"><p>${arr[i].description}</p></div><img src="${iconVert.src}" alt="vert" />`;
     list.appendChild(item);
   }
