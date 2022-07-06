df0ed7db86d617645b955156b2e01a9431187b4c
diego <diegoyon@outlook.com>
2022-07-06 14:39:06 -0600

    Add font-family style

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   devServer: {
-    static: './dist',
-    watchFiles: ['src/*.html'],
+    static: "./dist",
+    watchFiles: ["src/*.html"],
     port: 8080,
     open: true,
     compress: true,
     hot: true,
     liveReload: true,
   },
+  devtool: "source-map", //maps the correct line of error in the source code
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
+    assetModuleFilename: "[name][ext]", //makes the assets conserve their name when build
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.html$/i,
-        loader: 'html-loader',
+        loader: "html-loader",
       },
     ],
   },
